e1625a7f0a1157dcf0e1c63e7585e62fee4f526e
Fateh Sanat Derak <Fateh Sanat Derak@DESKTOP-F7M76PF>
2024-07-01 20:54:30 +0330

    Add project files.

---
+++ ForeFlightChart/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ForeFlightChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PnlBase = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.PnlBase);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 396);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // PnlBase
+            // 
+            this.PnlBase.BackColor = System.Drawing.SystemColors.Control;
+            this.PnlBase.Location = new System.Drawing.Point(31, 38);
+            this.PnlBase.Name = "PnlBase";
+            this.PnlBase.Size = new System.Drawing.Size(450, 300);
+            this.PnlBase.TabIndex = 0;
+            this.PnlBase.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PnlBase_MouseMove);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 396);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(671, 95);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 491);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PnlBase;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ForeFlightChart/Form1.cs
@@ -0,0 +1,94 @@
+﻿// رسم هایی که میکنم دست آخر باید بیارمش روی بیت مپ
+
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ForeFlightChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //char(System.Text.Encoding.GetEncoding(0227))
+            //0255---0227
+            label1.Font = new Font("ESRI Weather", 20);
+
+            label1.Text = ((char)0227).ToString();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Pen pn = new Pen(Color.FromArgb(195, 195, 195));
+            pn.DashStyle = DashStyle.Dash;
+            Graphics e3 = PnlBase.CreateGraphics();
+            for (int i = 0; i <= PnlBase.Height; i += 50)
+            {
+                if (i != PnlBase.Height)
+                    e3.DrawLine(pn, new Point(0, i), new Point(PnlBase.Width, i));
+                else
+                    e3.DrawLine(pn, new Point(0, i - 1), new Point(PnlBase.Width, i - 1)); // برای خط آخر که نشان نمیده هست
+            }
+            for (int j = 0; j <= PnlBase.Width; j += 50)
+            {
+                if (j != PnlBase.Width)
+                    e3.DrawLine(pn, new Point(j, 0), new Point(j, PnlBase.Height));
+                else
+                    e3.DrawLine(pn, new Point(j - 1, 0), new Point(j - 1, PnlBase.Height - 1));
+            }
+
+        }
+
+        private void PnlBase_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.Location.ToString();
+        }
+        int _rotcount = 0;
+        int _rotlog = 80;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Font fn = new Font("ESRI Weather", 40,FontStyle.Bold);
+            string text = ((char)0227).ToString();
+            Graphics gr = PnlBase.CreateGraphics();
+            GraphicsState st = gr.Save();
+            gr.ResetTransform();
+            if (_rotcount > 0)
+            {
+                _rotlog += 10;
+                gr.RotateTransform(_rotlog);
+                _rotcount++;
+            }
+            else
+            {
+                _rotlog += 10;
+                gr.RotateTransform(_rotlog);
+                _rotcount++;
+            }
+            gr.TranslateTransform((float)55, (float)50,MatrixOrder.Append);
+            Pen blackPen = new Pen(Color.Black, 3);
+            using (StringFormat sf = new StringFormat())
+            {
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+                gr.DrawString(text, fn, new SolidBrush(Color.Black), 0, 0, sf);
+            }
+            gr.Restore(st);
+
+
+
+        }
+    }
+}
